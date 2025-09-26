38136e8581fbcdade5b214610cfa5232f88e100c
Manan-Ag <164680274+Manan-Ag@users.noreply.github.com>
2025-09-26 14:27:08 -0600

    Manan's commit

--- app/src/main/java/com/example/oracle/Shape.java
+++ app/src/main/java/com/example/oracle/Shape.java
@@ -1,7 +1,11 @@
 package com.example.oracle;
-
 public abstract class Shape {
     public int x;
     public int y;
+
+    public Shape(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
     public String colour = "something";
 }
